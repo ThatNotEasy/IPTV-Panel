4189d9e24721af6b4526c1802742288adeddd592
unknown <apid9755@gmail.com>
2024-09-02 04:58:56 +0800

    improve

--- modules/authorized.py
+++ modules/authorized.py
@@ -38,78 +38,8 @@ class AUTHORIZED:
     
 # ==================================================================================================================================================== #
 
-    def update_expiration(self):
-        self.expired_at = self.calculate_expiration()
-        try:
-            db = SQLITE()  # Replace with your actual database connection
-            query = """
-                UPDATE users 
-                SET expired_at = ?
-                WHERE user_id = ?;
-            """
-            db.execute(sql=query, args=(self.expired_at, self.user_id))
-            db.commit()
-        
-        except Exception as e:
-            print(f"Error updating expiration: {str(e)}")
-        
-        finally:
-            db.close()
-            
-# ==================================================================================================================================================== #
 
-    def get_iptvs(self):
-        
-        self.playlist = f"http://127.0.0.1:1337/{self.user_id}/playlist.m3u"
-        
-        try:
-            db = SQLITE()
-            
-            query = "SELECT user_id, username FROM users WHERE user_id = ?;"
-            result = db.query(sql=query, args=(self.uuid,))
-            
-            if not result:
-                response = {
-                    "status": "error",
-                    "message": "No IPTVs found for the provided UUID.",
-                    "responseData": None
-                }
-                return jsonify(response), 404
 
-            response = {
-                "status": "success",
-                "message": "IPTVs retrieved successfully.",
-                "responseData": {
-                    "playlist_url": self.playlist
-                }
-            }
-            self.update_expiration()
-            return jsonify(response), 200
-        
-        except Exception as e:
-            response = {
-                "status": "error",
-                "message": f"Error retrieving IPTVs: {str(e)}",
-                "responseData": None
-            }
-            return jsonify(response), 500
-        
-        finally:
-            db.close()
-            
-    def check_user_id(self, user_id):
-        query = "SELECT 1 FROM users WHERE user_id = ? LIMIT 1;"
-        db = SQLITE()
-        try:
-            result = db.query(sql=query, args=(user_id,))
-            return len(result) > 0
-        except Exception as e:
-            print(f"Error checking user_id: {str(e)}")
-            return False
-        finally:
-            db.close()
-        
-# ==================================================================================================================================================== #
 
 
     
--- routes/users.py
+++ routes/users.py
@@ -1,11 +1,12 @@
 from flasgger import swag_from
 from modules.users import USERS
 from modules.config import setup_config
-from flask import Blueprint, request, jsonify, render_template, url_for
+from flask import Blueprint, request, jsonify, render_template, url_for, Response, abort, redirect
 from flask_cors import cross_origin, CORS
 from routes.authorized import admin_password
 from modules.authorized import AUTHORIZED
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+import os
 
 config = setup_config()
 ADMIN_PASS = config["ADMIN"]["PASSWORD"]
@@ -113,17 +114,32 @@ def update_single_user(user_id):
 
 @users_bp.route('/<user_id>/playlist', methods=['GET'])
 @cross_origin()
+# @jwt_required()
 @swag_from('../swagger/users/get_playlist.yml')
 def get_playlist(user_id):
     if not user_id:
         return jsonify({"responseData": "Missing required field!"}), 400
 
-    authorized = AUTHORIZED()
-    authorized.user_id = user_id
+    users = USERS()
+    users.user_id = user_id
     
-    user_exists = authorized.check_user_id(user_id)
+    user_exists = users.check_user_id(user_id)
     if not user_exists:
         return jsonify({"responseData": "User ID not found!"}), 404
+
+    # Detecting User-Agent and Client IP
+    user_agent = request.headers.get('User-Agent')
+    if "Mozilla/5.0" in user_agent:
+        return redirect(url_for('static', filename='img/pepes.gif'))
+        
+    client_ip = request.remote_addr
+
+    # Log or handle the detected User-Agent and IP address
+    print(f"User-Agent: {user_agent}")
+    print(f"Client IP: {client_ip}")
+
+    # Optional: Log or restrict based on user-agent and IP
+    # You can also store this information in logs or take specific actions based on it
 
     playlist_file_path = os.path.join("templates", "playlist.m3u")
     if not os.path.isfile(playlist_file_path):
