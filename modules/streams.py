c8d376397c14cb416b30239f922259ff2430ef28
Pari Malam <apid9755@gmail.com>
2025-01-12 06:44:40 +0800

    added playlist generator

--- modules/streams.py
+++ modules/streams.py
@@ -146,3 +146,103 @@ class STREAMS:
             db.close()
         
 # ============================================================================================================================================ #
+
+    def playlists(self):
+        if self.playlist_format == "1":
+            return self._generate_format_1_playlist()
+        elif self.playlist_format == "2":
+            return self._generate_format_2_playlist()
+        else:
+            return {"error": "Invalid playlist format specified"}, 400
+
+    def _generate_format_1_playlist(self):
+        self.logging.info("Generating Format 1 playlist")
+        headers_str = f"#EXTHTTP:{{\"User-Agent\":\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36\",\"Referer\":\"{self.referer}\"}}\n"
+        additional_lines = [
+            '#KODIPROP:inputstreamaddon=inputstream.adaptive',
+            '#KODIPROP:inputstream.adaptive.manifest_type=dash',
+            '#KODIPROP:inputstream.adaptive.license_type=org.w3.clearkey',
+        ]
+        if self.key and self.kid:
+            additional_lines.append(f'''#KODIPROP:inputstream.adaptive.license_key={{"keys":[{{"kty":"oct","kid":"{self.kid}","k":"{self.key}"}}],"type":"temporary"}}''')
+
+        playlist_content = headers_str
+        for line in additional_lines:
+            playlist_content += line + '\n'
+
+        playlist_entry = f"#EXTINF:-1 type=\"{self.genre_type}\" group-title=\"{self.channel_group}\" tvg-logo=\"{self.channel_thumbnail}\",{self.channel_name}\n"
+        playlist_entry += f"{self.mpd_url}\n\n"
+        playlist_content += playlist_entry
+
+        output_file = f"templates/generator/{self.content_id}.m3u"
+        try:
+            with open(output_file, "w") as f:
+                f.write("#EXTM3U\n")
+                f.write(playlist_content)
+        except Exception as e:
+            return {"error": "Error generating playlist"}, 500
+        return {"responseData": playlist_content, "filePath": f"{self.content_id}.m3u"}
+
+    def _generate_format_2_playlist(self):
+        self.logging.info("Generating Format 2 playlist")
+        headers_str = f"#EXTHTTP:{{\"User-Agent\":\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36\",\"Referer\":\"{self.referer}\"}}\n"
+        
+        # Define other playlist content
+        license_key = f"{self.kid}:{self.key}"
+        entry_str = f"#EXTINF:-1 type=\"{self.genre_type}\" group-title=\"{self.channel_group}\" tvg-logo=\"{self.channel_thumbnail}\",{self.channel_name}\n"
+        entry_str += "#KODIPROP:inputstreamaddon=inputstream.adaptive\n"
+        entry_str += "#KODIPROP:inputstream.adaptive.manifest_type=dash\n"
+        entry_str += "#KODIPROP:inputstream.adaptive.license_type=org.w3.clearkey\n"
+        entry_str += f"#KODIPROP:inputstream.adaptive.license_key={license_key}\n"
+        entry_str += headers_str  # Add headers here
+        entry_str += f"{self.mpd_url}\n\n"
+        
+        playlist_content = entry_str
+        output_file = f"templates/generator/{self.content_id}.m3u"
+        try:
+            with open(output_file, "w") as f:
+                f.write("#EXTM3U\n")
+                f.write(playlist_content)
+        except Exception as e:
+            return {"error": "Error generating playlist"}, 500
+        
+        return {"responseData": playlist_content, "filePath": f"{self.content_id}.m3u"}
+    
+    def generate_epg(self, channels, programs):
+        root = ET.Element("tv")
+        for channel in channels:
+            channel_elem = ET.SubElement(root, "channel", id=channel['id'])
+            ET.SubElement(channel_elem, "display-name").text = channel['name']
+
+        for program in programs:
+            program_elem = ET.SubElement(root, "programme", channel=program['channel'], start=program['start'], stop=program['end'])
+            ET.SubElement(program_elem, "title").text = program['title']
+            if 'desc' in program:
+                ET.SubElement(program_elem, "desc").text = program['desc']
+
+        tree = ET.ElementTree(root)
+        with open(self.output_file, 'wb') as file:
+            tree.write(file, encoding='utf-8', xml_declaration=True)
+        
+        return {"responseData": self.get_epg_content(), "filePath": self.output_file}
+
+    def get_epg_content(self):
+        with open(self.output_file, 'r', encoding='utf-8') as file:
+            return file.read()
+
+    def _generate_format_3_playlist(self):
+        data = {
+            "name": f"Updated: {self.name}",
+            "category": f"Updated: {self.category}",
+            "info": {
+                "poster": self.poster,
+                "bg": self.bg,
+                "plot": self.plot,
+                "backdrop": self.backdrop,
+                "director": self.director.split(','),
+                "cast": self.cast.split(','),
+                "year": self.year
+            },
+            "video": self.video
+        }
+        return json.dumps(data, indent=4)
--- routes/streams.py
+++ routes/streams.py
@@ -9,6 +9,108 @@ CORS(streams_bp)
 
 # ============================================================================================================================================ #
 
+@streams_bp.route('/playlist_generator', methods=['POST'])
+@cross_origin()
+@jwt_required()
+@swag_from('../templates/swagger/iptv/playlist_generator.yml')
+def playlist():
+    if not request.is_json:
+        return jsonify({"error": "Missing JSON in the request"}), 400
+
+    data = request.get_json()
+    playlist_format = data.get('playlistFormatter')
+    channel_name = data.get('name')
+    channel_thumbnail = data.get('thumbnailUrl')
+    channel_mpd = data.get('mpdUrl')
+    genre_type = data.get('genreType')
+    channel_group = data.get('groupName')
+    kid = data.get('kid')
+    key = data.get('key')
+    referer = data.get('referer')
+
+    if not all([playlist_format, channel_name, channel_thumbnail, channel_mpd, channel_group, genre_type, kid, key, referer]):
+        return jsonify({"error": "Missing required parameters"}), 400
+
+    try:
+        iptv = IPTV()
+        iptv.channel_name = channel_name
+        iptv.channel_thumbnail = channel_thumbnail
+        iptv.mpd_url = channel_mpd
+        iptv.channel_group = channel_group
+        iptv.genre_type = genre_type
+        iptv.referer = referer
+        iptv.kid = kid
+        iptv.key = key
+
+        if playlist_format == "1":
+            result = iptv._generate_format_1_playlist()
+        elif playlist_format == "2":
+            result = iptv._generate_format_2_playlist()
+        else:
+            return jsonify({"error": "Invalid playlist format specified"}), 400
+
+        return jsonify(result)
+
+    except Exception as e:
+        logging.error(f"Internal server error: {e}")
+        return jsonify({"error": "Internal server error"}), 500
+    
+    
+# ============================================================================================================================================ #
+
+@streams_bp.route('/downloadPlaylist', methods=['GET'])
+@cross_origin()
+@jwt_required()
+def download_playlist():
+    file_path = request.args.get('filePath')
+    
+    if not file_path:
+        return jsonify({"error": "File path parameter is required"}), 400
+    
+    safe_file_name = os.path.basename(file_path)
+    full_path = os.path.join('templates/generator', safe_file_name)
+    
+    if not os.path.exists(full_path):
+        return jsonify({"error": "File not found"}), 404
+    
+    logging.info(f"File found and ready to be downloaded: {full_path}")
+    return send_file(full_path, as_attachment=True, download_name=safe_file_name)
+
+# ============================================================================================================================================ #
+
+@streams_bp.route('/epg_generator', methods=['POST'])
+@cross_origin()
+@jwt_required()
+@swag_from('../templates/swagger/iptv/generate_epg.yml')
+def generate_epg_api():
+    data = request.json
+
+    if not data or 'channels' not in data or 'programs' not in data:
+        return jsonify({"error": "Missing channels or programs data"}), 400
+
+    channels = data['channels']
+    programs = data['programs']
+
+    epg_generator = IPTV()
+
+    output_file_name = 'epg.xml'
+    output_file_path = os.path.join('templates/generator', output_file_name)
+    
+    epg_generator.output_file = output_file_path
+    result = epg_generator.generate_epg(channels, programs)
+
+    os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+    
+    with open(output_file_path, 'w', encoding='utf-8') as file:
+        file.write(result['responseData'])
+
+    if not os.path.exists(output_file_path):
+        return jsonify({"error": "Failed to generate EPG file"}), 500
+
+    logging.info(f"EPG file generated and ready to be downloaded: {output_file_path}")
+    return send_file(output_file_path, as_attachment=True, download_name=os.path.basename(output_file_path))
+
+
 @streams_bp.route('/get_streams', methods=['GET'])
 @cross_origin()
 @swag_from('../templates/swagger/streams/get_streams.yml')
